c2768bee7fcb374eca59200bce28326ee09dceb8
Kalashin <leonardoserbim@gmail.com>
2018-07-12 17:41:10 -0300

    ng-bootstrap import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropdownBasicComponent } from './dropdown-basic/dropdown-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownBasicComponent
   ],
   imports: [
     BrowserModule
